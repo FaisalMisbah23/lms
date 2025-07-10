01c0510e042cadbd34e1888083048525dd768294
Faisal Misbah <faisalmisbah23@gmail.com>
2025-07-11 01:15:04 +0500

    fix: remove unused parameter and error logging

--- client/redux/features/api/apiSlice.ts
+++ client/redux/features/api/apiSlice.ts
@@ -8,14 +8,14 @@ export const apiSlice = createApi({
   }),
   endpoints: (builder) => ({
     refreshToken: builder.query({
-      query: (data) => ({
+      query: () => ({
         url: "refresh-token",
         method: "POST",
         credentials: "include" as const,
       }),
     }),
     loadUser: builder.query({
-      query: (data) => ({
+      query: () => ({
         url: "me",
         method: "GET",
         credentials: "include" as const,
@@ -30,7 +30,7 @@ export const apiSlice = createApi({
             })
           );
         } catch (error: any) {
-          console.log(error);
+          // console.log(error);
         }
       },
     }),
